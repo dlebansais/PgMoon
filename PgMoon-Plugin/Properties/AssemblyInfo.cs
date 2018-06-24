0cebc59acb53e3d7cd8366fb1afe9208200e84b4
dlebansais <dlebansais@gmail.com>
2018-06-24 13:03:47 +0200

    Fixed the main icon.

--- PgMoon-Plugin/Properties/AssemblyInfo.cs
+++ PgMoon-Plugin/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.205")]
-[assembly: AssemblyFileVersion("1.0.0.22")]
+[assembly: AssemblyVersion("1.1.0.207")]
+[assembly: AssemblyFileVersion("1.0.0.23")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
