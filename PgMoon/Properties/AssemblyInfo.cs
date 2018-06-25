56d4ab1465acc14c9ede9e7e000bfa6c6abba888
dlebansais <dlebansais@gmail.com>
2018-06-25 23:05:50 +0200

    Updated the version number.

--- PgMoon/Properties/AssemblyInfo.cs
+++ PgMoon/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.210")]
-[assembly: AssemblyFileVersion("1.1.0.209")]
+[assembly: AssemblyVersion("1.1.0.212")]
+[assembly: AssemblyFileVersion("1.1.0.210")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
