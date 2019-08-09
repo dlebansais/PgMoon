0da28d2c38416142b824471383ee08ad5f4b937f
dlebansais <dlebansais@gmail.com>
2019-08-09 21:29:58 +0200

    Removed Fody.

--- SinglePluginHost/Properties/AssemblyInfo.cs
+++ SinglePluginHost/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.301")]
-[assembly: AssemblyFileVersion("1.1.0.271")]
+[assembly: AssemblyVersion("1.1.0.302")]
+[assembly: AssemblyFileVersion("1.1.0.272")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
