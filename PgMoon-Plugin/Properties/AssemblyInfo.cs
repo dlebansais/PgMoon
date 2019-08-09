cd836edc54dee7417874edc9ada961423d6c1e3a
dlebansais <dlebansais@gmail.com>
2019-08-09 21:42:29 +0200

    Reinstated Fody.

--- PgMoon-Plugin/Properties/AssemblyInfo.cs
+++ PgMoon-Plugin/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.304")]
-[assembly: AssemblyFileVersion("1.1.0.299")]
+[assembly: AssemblyVersion("1.1.0.306")]
+[assembly: AssemblyFileVersion("1.1.0.300")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
