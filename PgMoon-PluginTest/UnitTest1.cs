0b053b5a58a0236ce2111e738907bcfa195e4968
Chuddington <6558350+Chuddington@users.noreply.github.com>
2021-04-11 13:25:31 +0100

    Add Test library for PgMoon-Plugin

---
+++ PgMoon-PluginTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PgMoon_PluginTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
